d8efafc83fe73d3c7c76e15522d77be3cb358b04
abdssamie <abdessamie.elmoubarki@gmail.com>
2025-12-25 00:00:36 +0100

    Revert "feat: add 3 showcase workflows (content repurposing, lead enrichment, expense auditor)"
    This reverts commit de82cfc4c73d1fc38a90bd4fe4e8df651820826c.

--- apps/inngest-app/src/inngest/functions/index.ts
+++ apps/inngest-app/src/inngest/functions/index.ts
@@ -4,9 +4,6 @@ import { randomTextLoopWorkflow } from "./random-text-loop/function";
 import { postToSlackWorkflow } from "./post-to-slack/function";
 import { scrapeWebsiteWorkflow } from "./scrape-website/function";
 import { sendOutlookEmailWorkflow } from "./send-outlook-email/function";
-import { contentRepurposingWorkflow } from "./content-repurposing/function";
-import { leadEnrichmentWorkflow } from "./lead-enrichment/function";
-import { expenseAuditorWorkflow } from "./expense-auditor/function";
 import { workflowStatusHandler } from "./system/workflow-status-handler";
 import { automationMetricsAggregation, manualMetricsAggregation } from "./automation-metrics-aggregation";
 import { 
@@ -20,10 +17,7 @@ export const workflowFunctions = [
     randomTextLoopWorkflow,
     postToSlackWorkflow,
     scrapeWebsiteWorkflow,
-    sendOutlookEmailWorkflow,
-    contentRepurposingWorkflow,
-    leadEnrichmentWorkflow,
-    expenseAuditorWorkflow
+    sendOutlookEmailWorkflow
 ];
 
 export const systemFunctions = [
